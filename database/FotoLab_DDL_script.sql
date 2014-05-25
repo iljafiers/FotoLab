41aa0d0c8f136efe5350c9789c0731c77c41b1eb
Pieter Scheffers <pieter.scheffers@gmail.com>
2014-05-25 17:28:47 +0200

    Database scripts aangepast + webservice project aangemaakt

--- database/FotoLab_DDL_script.sql
+++ database/FotoLab_DDL_script.sql
@@ -26,9 +26,28 @@ go
 /*==============================================================*/
 /* Table: Gebruikers                                       */
 /*==============================================================*/
-create table Klanten (
-   id               	numeric(5)           primary key identity,
-   naam					varchar(45)			 not null
+create table klanten (
+   id               	int           		primary key identity not null,
+   naam					varchar(45)			not null
 )
 go
 
+create table fotoserie (
+	id 					int 				primary key identity not null,
+	serie_key			varchar(50)			not null 				
+)
+go
+
+ -- MAX path limit: http://stackoverflow.com/questions/265769/maximum-filename-length-in-ntfs-windows-xp-and-windows-vista
+create table foto (
+	id 					int 				primary key identity not null,
+	fotoserie_id		int 				not null,
+	path				varchar(260)		not null
+)
+go
+
+create table klant_key (
+	klant_id			int 				not null,
+	fotoserie_id		int 				not null
+)
+go
--- database/FotoLab_Insert_script.sql
+++ database/FotoLab_Insert_script.sql
@@ -7,10 +7,15 @@ use fotolabdatabase
 go
 
 -- sloop alles
-TRUNCATE Klanten;
+TRUNCATE TABLE klanten;
+TRUNCATE TABLE fotoserie;
+TRUNCATE TABLE foto;
+TRUNCATE TABLE klant_key;
 
 /* Opmerking:
    Een enkel aanhalingsteken binnen een string (zoals in muziekschool 2 en stuk 12) moet
    twee keer worden genoteerd; anders wordt het opgevat als einde-string teken.
 */
-INSERT INTO Klanten VALUES (1, 'Pieter Scheffers');
+INSERT INTO klanten VALUES ('Pieter Scheffers');
+INSERT INTO klanten VALUES ('Ilja Fiers');
+go
