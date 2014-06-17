aec18d58dd062070f93f1721892aa99bf744fcd0
Ilja Fiers <i.fiers@spiritit.com>
2014-06-17 17:03:56 +0200

    De eerste versie van het consumeren van een webservice lijkt te werken.

--- FotoProducent/Form1.cs
+++ FotoProducent/Form1.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
+using System.Web;
+using System.Web.Script.Serialization;
 
 namespace FotoProducent
 {
@@ -14,6 +18,51 @@ namespace FotoProducent
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnChangedKlantID(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(this.numericUpDownKlantID.Value);
+
+            string url = "http://localhost:2372/api/Klant/" + id;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                string JSON;
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    JSON =  reader.ReadToEnd();
+                }
+
+                // parse JSON
+                var serializer = new JavaScriptSerializer();
+                Klant kl = serializer.Deserialize<Klant>(JSON);
+
+                if (kl != null)
+                {
+                    this.KlantNaam.Text = kl.Naam;
+                }
+
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                throw;
+            }
         }
     }
 }
